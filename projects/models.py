bd347784c61985ac1f3c7bddf05284153fc934a4
rotbauma <rtbauman@hawkmail.hfcc.edu>
2024-07-02 17:58:50 -0400

    Created database and added projects

---
+++ projects/models.py
@@ -0,0 +1,10 @@
+#projects/models.py
+
+from django.db import models
+
+# Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
